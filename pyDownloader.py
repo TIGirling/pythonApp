0c37bff8a9fa6c539b5e30f4a96dc97b57ffdefb
Tim Girling <TIGirling@googlemail.com>
2017-10-23 17:19:59 +0100

    Add download from URL functionality

--- pyDownloader.py
+++ pyDownloader.py
@@ -2,6 +2,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
+import urllib.request
 
 
 class Downloader(QDialog):
@@ -11,26 +12,42 @@ class Downloader(QDialog):
 
         layout = QVBoxLayout()
 
-        url = QLineEdit()
-        save_location = QLineEdit()
-        progress = QProgressBar()
+        self.url = QLineEdit()
+        self.save_location = QLineEdit()
+        self.progress = QProgressBar()
         download = QPushButton("Download")
 
-        url.setPlaceholderText("Type URL Here")
-        save_location.setPlaceholderText("File Save Location")
+        self.url.setPlaceholderText("Type URL Here")
+        self.save_location.setPlaceholderText("File Save Location")
 
-        progress.setValue(0)
-        progress.setAlignment(Qt.AlignCenter)
+        self.progress.setValue(0)
+        self.progress.setAlignment(Qt.AlignCenter)
 
-        layout.addWidget(url)
-        layout.addWidget(save_location)
-        layout.addWidget(progress)
+        layout.addWidget(self.url)
+        layout.addWidget(self.save_location)
+        layout.addWidget(self.progress)
         layout.addWidget(download)
 
         self.setLayout(layout)
 
         self.setWindowTitle("PyDownloader")
         self.setFocus()
+
+        download.clicked(self.download)
+
+    def download(self):
+        url = self.url.text()
+        save_location = self.save_location.text()
+        urllib.request.urlretrieve(url, save_location, self.report)
+
+
+    def report(self, blocknum, blocksize, totalsize):
+        readSoFar = blocknum * blocksize
+        if totalsize > 0:
+            percent = readSoFar * 100 / totalsize
+            self.progress.setValue(int(percent))
+
+
 
 app = QApplication(sys.argv)
 dl = Downloader()
